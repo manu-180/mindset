2be42b00d9454e4c77b6e5f468bd848c25ff061f
manu-180 <manumanu97@hotmail.com>
2025-11-10 01:22:17 -0300

    actualizando pagina para vercel

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "437034758a8b27201b7b6ee277a0043a
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "6aa461a40509b3deeba3efce2a44434a",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "e8abf30341e3ab1358d23b9bca8435a8",
+"flutter_bootstrap.js": "3830619d4073d888ae6ab41e10412d68",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
